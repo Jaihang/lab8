00ca0f5671eb0bb7c0fb384f4dec864481b3122d
Aihang Jin <aijin@ucsd.edu>
2021-02-25 17:33:41 -0800

    updated

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "yPL89UCC36QX3BPZ3y8oBGyOM2EGRAFh";
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
